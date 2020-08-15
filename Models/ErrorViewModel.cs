12c81c8dcef6a8037e6e65da4b35eedd9c1f6d48
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2020-08-15 18:48:04 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebEncryption.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
